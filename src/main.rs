1534feff129b62fc3085e04a775cb528b4151318
JS <minecraftprofile52@gmail.com>
2024-09-03 16:41:45 +0300

    Refactor and added memory location indicator to each line

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
 
 const BYTES_IN_LINE: usize = 16;
-const DEFAULT_PREVIEW_CHARACTER: u8 = 32;
-const NOT_PRINTABLE_CHARACTER: u8 = 46;
+const WHITESPACE_ASCII: u8 = 32;
+const NOT_PRINTABLE_ASCII: u8 = 46;
+
+/// Checks if the byte is printable ascii character
+fn is_printable_ascii(c: u8) -> bool {
+    return c >= 32 && c <= 127;
+}
+
+/// Print the bytes in hexadecimal and show ASCII preview
+fn hexdump(bytes: Vec<u8>) {
+    let mut preview_buffer: [u8; BYTES_IN_LINE] = [WHITESPACE_ASCII; BYTES_IN_LINE];
+    for (i, c) in bytes.iter().enumerate() {
+        if i % BYTES_IN_LINE == 0 {
+            print!("{:08X?}  ", i);
+        }
+
+        preview_buffer[i % BYTES_IN_LINE] = if is_printable_ascii(*c) { *c } else { NOT_PRINTABLE_ASCII };    
+        print!("{:02X?} ", c);
+
+        if i == bytes.len() - 1 && (i + 1) % BYTES_IN_LINE != 0 {
+            let fill_amount = BYTES_IN_LINE - ((i + 1) % BYTES_IN_LINE);
+            for _ in 0..fill_amount {
+                print!("   ");
+            }
+        }
+
+        if (i + 1) % BYTES_IN_LINE == 0 || i == bytes.len() - 1 {
+            let s = String::from_utf8(preview_buffer.to_vec()).unwrap();
+            print!(" | {} |", s);
+            print!("\n");
+
+            for i in 0..preview_buffer.len() {
+                preview_buffer[i] = WHITESPACE_ASCII;
+            }
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
@@ -14,28 +49,9 @@ fn main() {
                         println!("FAILED: Cannot read a directory!");
                         return;
                     }
-        
+                    
                     let byte_array = std::fs::read(absolute_path).unwrap();
-                    let mut preview_buffer: [u8; BYTES_IN_LINE] = [DEFAULT_PREVIEW_CHARACTER; BYTES_IN_LINE];
-                    for (index, value) in byte_array.iter().enumerate() {
-                        preview_buffer[index % BYTES_IN_LINE] = if *value >= 32 && *value <= 127 { *value } else { NOT_PRINTABLE_CHARACTER };    
-                        print!("{:02X?} ", value);
-                        if index == byte_array.len() - 1 {
-                            let fill_amount = BYTES_IN_LINE - ((index + 1) % BYTES_IN_LINE);
-                            for _ in 0..fill_amount {
-                                print!("   ");
-                            }
-                        }
-                        if (index + 1) % BYTES_IN_LINE == 0 || index == byte_array.len() - 1 {
-                            let s = String::from_utf8(preview_buffer.to_vec()).unwrap();
-                            print!("| {} |", s);
-                            print!("\n");
-        
-                            for i in 0..preview_buffer.len() {
-                                preview_buffer[i] = DEFAULT_PREVIEW_CHARACTER;
-                            }
-                        }
-                    }
+                    hexdump(byte_array);
                 },
                 Err(_) => {
                     println!("FAILED: Invalid filepath was given!");
